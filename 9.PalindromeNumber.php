98a6c54e7febe0b859328820503d415705b87ba5
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2024-08-11 17:19:41 +0600

    Update 9.PalindromeNumber.php

--- 9.PalindromeNumber.php
+++ 9.PalindromeNumber.php
@@ -1,1 +1,38 @@
 
+//first Solution
+
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        return $x == strrev($x);
+    }
+}
+
+
+//second Solution
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        if ($x < 0) {
+            return false;
+        }
+        
+        $reversed = 0;
+        $original = $x;
+        
+        while ($x > 0) {
+            $reversed = $reversed * 10 + $x % 10;
+            $x = (int)($x / 10);
+        }
+        
+        return $original == $reversed;
+    }
+}
