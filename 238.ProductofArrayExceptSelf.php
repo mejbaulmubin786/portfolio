9f739c352c463b9f2bed0ab77ffd0a95d09a1e7f
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2024-08-12 20:46:40 +0600

    Create 238.ProductofArrayExceptSelf.php

---
+++ 238.ProductofArrayExceptSelf.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution {
+
+    function productExceptSelf($nums) {
+        $n = count($nums);
+        $result = array_fill(0, $n, 1);
+
+        // Prefix products
+        $prefix = 1;
+        for ($i = 0; $i < $n; $i++) {
+            $result[$i] *= $prefix;
+            $prefix *= $nums[$i];
+        }
+
+        // Suffix products and final result
+        $suffix = 1;
+        for ($i = $n - 1; $i >= 0; $i--) {
+            $result[$i] *= $suffix;
+            $suffix *= $nums[$i];
+        }
+
+        return $result;
+    }
+}
