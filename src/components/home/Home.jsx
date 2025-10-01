354bc2cf445929c54b2ea7a8ae718d5b518bd41a
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-01 20:44:38 +0600

    TextChange added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import avatarImg from "../../assets/7358602-removebg-preview.png"
+import TextChange from '../TextChange'
 
 const Home = () => {
     return (
         <div className='text-white flex w-full justify-between items-start p-10 md:p-20'>
             <div className='md:w-2/4 md:pt-10'>
-                <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter'>Hello' I am Mubin</h1>
+                <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter'>
+                    <TextChange/>
+                </h1>
                 <p className='text-sm md:text-2xl tracking-tight'>Lorem ipsum dolor sit amet consectetur
                     adipisicing elit. Suscipit dolorem, facere error odio aperiam iure numquam! Voluptates ut
                     placeat rerum enim, voluptatum est reprehenderit voluptas non officia possimus? Reiciendis, nam.
