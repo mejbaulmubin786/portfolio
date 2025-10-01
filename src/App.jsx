0a0e2b01d5595bc3dee6fbed0f3137660235a43c
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-01 18:54:49 +0600

    update about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/home/Home'
+import About from './components/about/About'
 
 const App = () => {
   return (
     <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
       <Navbar />
       <Home />
+      <About />
     </div>
   )
 }
