01538864a84c68fc0462d2991a0d8e617b1e69c0
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-01 20:13:59 +0600

    Experience page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/home/Home'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Home />
       <About />
+      <Experience />
     </div>
   )
 }
